d6a5a5d6e27804edde9ebac2178f5ee75c2b3de1
LinaShadrach <lina.shadrach@gmail.com>
2017-12-15 16:05:21 -0800

    add Item model

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ToDoList.Models
 {
+    [Table("Items")]
     public class Item
     {
-        public Item()
-        {
-        }
+        [Key]
+        public int ItemId { get; set; }
+        public string Description { get; set; }
     }
 }
